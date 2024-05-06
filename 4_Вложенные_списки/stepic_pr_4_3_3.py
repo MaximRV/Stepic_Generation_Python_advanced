31347a3997a5b4feeba5b57aec3ccb1a0545d474
Maxim Ryndin <150359327+MaximRV@users.noreply.github.com>
2024-05-06 22:17:34 +0500

    Треугольник Паскаля 1 🌶️
    Треугольник Паскаля — бесконечная таблица биномиальных коэффициентов, имеющая треугольную форму. В этом треугольнике на вершине и по бокам стоят единицы. Каждое число равно сумме двух расположенных над ним чисел.
    0:      1
    1:     1 1
    2:    1 2 1
    3:   1 3 3 1
    4:  1 4 6 4 1
          .....
    На вход программе подается число n. Напишите программу, которая возвращает указанную строку треугольника Паскаля в виде списка (нумерация строк начинается с нуля).
    Формат входных данных
    На вход программе подается число (n≥0).
    Формат выходных данных
    Программа должна вывести указанную строку треугольника Паскаля в виде списка.
    Примечание 1. Решение удобно оформить в виде функции pascal(), которая принимает номер строки и возвращает соответствующую строку треугольника Паскаля.
    Тестовые данные 🟢
    Sample Input 1:
    0
    Sample Output 1:
    [1]
    Sample Input 2:
    1
    Sample Output 2:
    [1, 1]
    Sample Input 3:
    2
    Sample Output 3:
    [1, 2, 1]
    Sample Input 4:
    3
    Sample Output 4:
    [1, 3, 3, 1]

---
+++ 4_Вложенные_списки/stepic_pr_4_3_3.py
@@ -0,0 +1,25 @@
+def pascal(n):
+    result = [1]
+    len_str = n + 1
+
+    for i in range(1, n):
+        elem1 = 1
+        for j in range(1, n + 1):
+            elem1 = elem1 * j
+
+        elem2 = 1
+        for k in range(1, i + 1):
+            elem2 = elem2 * k
+
+        elem3 = 1
+        for h in range(1, n - i + 1):
+            elem3 = elem3 * h
+
+        result.append(elem1 // (elem2 * elem3))
+    if n != 0:
+        result.append(1)
+
+    return result
+
+n = int(input())
+print(pascal(n))
