6aacd5bf01d2a0ba11772e94bdc786d9633ebe9c
Maxim Ryndin <150359327+MaximRV@users.noreply.github.com>
2024-05-15 19:54:10 +0300

    Максимальный в области 1
    Напишите программу, которая выводит максимальный элемент в заштрихованной области квадратной матрицы.
    Формат входных данных
    На вход программе подаётся натуральное число n — количество строк и столбцов в матрице, затем элементы матрицы (целые числа) построчно через пробел.
    Формат выходных данных
    Программа должна вывести одно число — максимальный элемент в заштрихованной области квадратной матрицы.
    Примечание. Элементы главной диагонали также учитываются.
    Тестовые данные 🟢
    Sample Input 1:
    3
    1 4 5
    6 7 8
    1 1 6
    Sample Output 1:
    7
    Sample Input 2:
    4
    0 1 4 6
    0 0 2 5
    0 0 0 7
    0 0 0 0
    Sample Output 2:
    0
    Sample Input 3:
    2
    6 0
    7 9
    Sample Output 3:
    9
    Sample Input 4:
    3
    -50 -10 -20
    -19 -78 -70
    -11 -12 -19
    Sample Output 4:
    -11

---
+++ 4_Вложенные_списки/stepic_pr_4_4_5.py
@@ -0,0 +1,17 @@
+n = int(input())
+matrix = []
+
+for _ in range(n):
+    row = [int(i) for i in input().split()]
+    matrix.append(row)
+
+max_elem = matrix[0][0]
+
+for i in range(n):
+    for j in range(n):
+        if (i >= j and i <= n - 1 -j) or (i >= j and  i >= n - 1 - j):
+            if matrix[i][j] > max_elem:
+                max_elem = matrix[i][j]
+
+print(max_elem)
+
