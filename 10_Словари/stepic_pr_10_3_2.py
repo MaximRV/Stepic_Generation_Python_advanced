65eb453a0067bc1f345ee017d7642ca8a6e6e85d
Maxim Ryndin <150359327+MaximRV@users.noreply.github.com>
2024-06-25 22:25:25 +0500

    Дополните приведенный код-5
    Дополните приведенный код так, чтобы он объединил содержимое двух словарей dict1 и dict2: если ключ есть в обоих словарях, добавьте его в результирующий словарь со значением, равным сумме соответствующих значений из первого и второго словаря; если ключ есть только в одном из словарей, добавьте его в результирующий словарь с его текущим значением. Результирующий словарь необходимо присвоить переменной result.
    
    Примечание. Выводить содержимое словаря result не нужно.

---
+++ 10_Словари/stepic_pr_10_3_2.py
@@ -0,0 +1,8 @@
+dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+
+for i in dict2:
+    if i in dict1:
+        dict2[i] = dict2[i] + dict1[i]
+dict1.update(dict2)
+result = dict1
