1c2661305319c14a89c52184c2e4301d9b53ac31
Maxim Ryndin <150359327+MaximRV@users.noreply.github.com>
2024-09-02 22:47:08 +0500

    Сортируй как хочешь
    Список athletes содержит сведения о спортсменах в виде кортежей: (имя, возраст, рост, вес).
    
    Напишите программу сортировки списка спортсменов по указанному полю:
    
    
    1: по имени;
    2: по возрасту;
    3: по росту;
    4: по весу.
    
    Формат входных данных
    На вход программе подается натуральное число от 1 до 4,   4 – номер поля по которому требуется отсортировать список.
    
    Формат выходных данных
    
    Программа должна вывести отсортированный по заданному полю список в соответствии с примерами.
    
    Примечание. Решите задачу без использования условного оператора.
    
    Тестовые данные 🟢
    
    Sample Input 1:
    
    3
    
    Sample Output 1:
    
    Рустам 10 128 30
    Дима 10 130 35
    Тимур 11 135 39
    Руслан 9 140 33
    Матвей 17 168 68
    Амир 16 170 70
    Рома 16 188 100
    Петя 15 190 90
    
    Sample Input 2:
    
    2
    
    Sample Output 2:
    
    Руслан 9 140 33
    Дима 10 130 35
    Рустам 10 128 30
    Тимур 11 135 39
    Петя 15 190 90
    Амир 16 170 70
    Рома 16 188 100
    Матвей 17 168 68

---
+++ 15_Функции/stepic_pr_15_4_4.py
@@ -0,0 +1,12 @@
+athletes = [('Дима', 10, 130, 35), ('Тимур', 11, 135, 39), ('Руслан', 9, 140, 33), ('Рустам', 10, 128, 30),
+            ('Амир', 16, 170, 70), ('Рома', 16, 188, 100), ('Матвей', 17, 168, 68), ('Петя', 15, 190, 90)]
+
+command = int(input())
+
+
+def sort_key(elem: tuple) -> str:
+    return elem[command - 1]
+
+
+for athlete in sorted(athletes, key=sort_key):
+    print(*athlete)
