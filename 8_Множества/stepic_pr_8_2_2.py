33e2845dc8ff5caecc38243413a20ed02bd4915f
Maxim Ryndin <150359327+MaximRV@users.noreply.github.com>
2024-10-26 20:51:18 +0500

    Книги на прочтение 🌶️
    Ученики 10-го класса онлайн-школы BEEGEEK получили задание прочесть на летних каникулах три книги:
    
    "Что такое математика?";
    "Математическая составляющая";
    "100 гениальных идей по математике".
    Оказалось, что n учеников прочитали первую книгу, m учеников — вторую, k учеников — третью. Также известно, что x учеников прочли первую или вторую, или обе эти книги, y учеников — вторую или третью, или обе, z учеников — первую и третью, или хотя бы одну из этих двух книг. Полностью выполнили задание только t учеников. Всего в 10-м классе учится "a" учеников. Напишите программу, которая выводит сколько учеников:
    
    прочитали только одну книгу
    прочитали только две книги
    не прочитали ни одной из рекомендованных книг
    
    Формат входных данных
    На вход программе подаются числа n,m,k,x,y,z,t,a каждое на отдельной строке.
    
    Формат выходных данных
    
    Программа должна вывести три числа в соответствии с условием задачи, каждое на отдельной строке.

---
+++ 8_Множества/stepic_pr_8_2_2.py
@@ -0,0 +1,8 @@
+n,m,k,x,y,z,t,a = (int(input()) for _ in range(8))
+b = x + y + z - n - m - k + t
+task1 = 3 * b - x - y -z
+task2 = b - f1 - t
+task3 = a - b
+print(task1)
+print(task2)
+print(task3)
