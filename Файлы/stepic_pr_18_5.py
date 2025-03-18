aed0f71711d717112038ee3b48e2b2441db0996a
Maxim Ryndin <150359327+MaximRV@users.noreply.github.com>
2025-03-18 18:48:18 +0500

    Tail of a File
    На вход программе подается строка текста с именем текстового файла. Напишите программу, выводящую на экран последние 10 строк данного файла.
    
    Формат входных данных
    
    На вход программе подается строка текста с именем существующего текстового файла.
    
    Формат выходных данных
    
    Программа должна вывести последние 10 строк этого файла.
    
    Примечание 1. Считайте, что исполняемая программа и файл находятся в одной папке.
    
    Примечание 2. Если количество строк в файле меньше 10, необходимо вывести содержимое файла полностью.
    
    Примечание 3. Если бы файл содержал строки:
    
    there are many different holidays
    on the first of january we
    celebrate new year on the
    seventh of january and the
    twenty-fifth of december we
    have christmas the twenty-third
    of february is the day of the
    defenders of the motherland
    or the army day then comes
    easter and radonitsa the
    first of may is the labour
    day the ninth of may is
    victory day the third of july
    is independence day then comes
    the seventh of november the day
    of the october revolution and so on
    
    то результатом будет:
    
    of february is the day of the
    defenders of the motherland
    or the army day then comes
    easter and radonitsa the
    first of may is the labour
    day the ninth of may is
    victory day the third of july
    is independence day then comes
    the seventh of november the day
    of the october revolution and so on
    
    Примечание 4. Подумайте над ситуацией, когда файл очень большой и нерационально считывать все его содержимое в память компьютера.

---
+++ Файлы/stepic_pr_18_5.py
@@ -0,0 +1,13 @@
+file_name = input()
+with open(file_name, 'r', encoding='utf-8') as file:
+    cursors_list = [0]
+    nextСursor = file.readline()
+    while nextСursor:
+        cursors_list.append(file.tell())
+        nextСursor = file.readline()
+    file.seek(0)
+    if len(cursors_list) <= 10:
+        print(file.read())
+    else:
+        file.seek(cursors_list[-11])
+        print(file.read())
